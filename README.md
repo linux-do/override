e64c8cc4e62613ea8c0b5ff056b1aea5d13dd9dc
wozulong <>
2024-05-16 18:16:53 +0800

    first commit
    Signed-off-by: wozulong <>

---
+++ README.md
@@ -0,0 +1,47 @@
+# Override
+
+## 这个仓库什么也不能做，请不要盯着我。
+
+VSCode 配置：
+
+```json
+    "github.copilot.advanced": {
+        "debug.overrideProxyUrl": "http://localhost:8181",
+        "debug.chatOverrideProxyUrl": "http://localhost:8181/v1/chat/completions",
+        "authProvider": "github-enterprise"
+    },
+    "github-enterprise.uri": "https://cocopilot.org",
+```
+
+其中 `http://localhost:8181` 是你启动的 `override` 服务地址。
+
+
+config.json 配置
+
+```json
+{
+  "bind": "127.0.0.1:8181",
+  "proxy_url": "",
+  "timeout": 600,
+  "codex_api_base": "https://api-proxy.oaipro.com/v1",
+  "codex_api_key": "sk-xxx",
+  "codex_api_organization": "",
+  "codex_api_project": "",
+  "chat_api_base": "https://api-proxy.oaipro.com/v1",
+  "chat_api_key": "sk-xxx",
+  "chat_api_organization": "",
+  "chat_api_project": "",
+  "chat_model_default": "gpt-4o",
+  "chat_model_map": {}
+}
+```
+
+`organization` 和 `project` 除非你有，且知道怎么会是再填。
+
+`chat_model_map` 是个模型映射的字典。会将请求的模型映射到你想要的，如果不存在映射，则使用 `chat_model_default` 。
+
+1. 理论上，Chat 部分可以使用 `chat2api` ，而 Codex 代码生成部分则不太适合使用 `chat2api` 。
+2. 代码生成部分做过延时生成和客户端 Cancel 处理，很有效节省你的Token。
+3. 我目前就试了下 `VSCode` ，至于 `JetBrains` 等IDE尚未适配，如果你有相关经验，请告诉我。
+4. 项目基于 `MIT` 协议发布，你可以修改，请保留原作者信息。
+5. 有什么问题，请在论坛 https://linux.do 讨论，欢迎PR。
---
+++ main.go
@@ -0,0 +1,264 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"errors"
+	"github.com/gin-gonic/gin"
+	"github.com/tidwall/gjson"
+	"github.com/tidwall/sjson"
+	"golang.org/x/net/http2"
+	"io"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+	"time"
+)
+
+type config struct {
+	Bind                 string            `json:"bind"`
+	ProxyUrl             string            `json:"proxy_url"`
+	Timeout              int               `json:"timeout"`
+	CodexApiBase         string            `json:"codex_api_base"`
+	CodexApiKey          string            `json:"codex_api_key"`
+	CodexApiOrganization string            `json:"codex_api_organization"`
+	CodexApiProject      string            `json:"codex_api_project"`
+	ChatApiBase          string            `json:"chat_api_base"`
+	ChatApiKey           string            `json:"chat_api_key"`
+	ChatApiOrganization  string            `json:"chat_api_organization"`
+	ChatApiProject       string            `json:"chat_api_project"`
+	ChatModelDefault     string            `json:"chat_model_default"`
+	ChatModelMap         map[string]string `json:"chat_model_map"`
+}
+
+func readConfig() *config {
+	content, err := os.ReadFile("config.json")
+	if nil != err {
+		log.Fatal(err)
+	}
+
+	_cfg := &config{}
+	err = json.Unmarshal(content, &_cfg)
+	if nil != err {
+		log.Fatal(err)
+	}
+
+	return _cfg
+}
+
+func getClient(cfg *config) (*http.Client, error) {
+	transport := &http.Transport{
+		ForceAttemptHTTP2: true,
+		DisableKeepAlives: false,
+	}
+
+	err := http2.ConfigureTransport(transport)
+	if nil != err {
+		return nil, err
+	}
+
+	if "" != cfg.ProxyUrl {
+		proxyUrl, err := url.Parse(cfg.ProxyUrl)
+		if nil != err {
+			return nil, err
+		}
+
+		transport.Proxy = http.ProxyURL(proxyUrl)
+	}
+
+	client := &http.Client{
+		Transport: transport,
+		Timeout:   time.Duration(cfg.Timeout) * time.Second,
+	}
+
+	return client, nil
+}
+
+func abortCodex(c *gin.Context, status int) {
+	c.Header("Content-Type", "text/event-stream")
+
+	c.String(status, "data: [DONE]\n")
+	c.Abort()
+}
+
+func closeIO(c io.Closer) {
+	err := c.Close()
+	if nil != err {
+		log.Println(err)
+	}
+}
+
+type ProxyService struct {
+	cfg    *config
+	client *http.Client
+}
+
+func NewProxyService(cfg *config) (*ProxyService, error) {
+	client, err := getClient(cfg)
+	if nil != err {
+		return nil, err
+	}
+
+	return &ProxyService{
+		cfg:    cfg,
+		client: client,
+	}, nil
+}
+
+func (s *ProxyService) InitRoutes(e *gin.Engine) {
+	e.POST("/v1/chat/completions", s.completions)
+	e.POST("/v1/engines/copilot-codex/completions", s.codeCompletions)
+}
+
+func (s *ProxyService) completions(c *gin.Context) {
+	ctx := c.Request.Context()
+
+	body, err := io.ReadAll(c.Request.Body)
+	if nil != err {
+		c.AbortWithStatus(http.StatusBadRequest)
+		return
+	}
+
+	model := gjson.GetBytes(body, "model").String()
+	if mapped, ok := s.cfg.ChatModelMap[model]; ok {
+		model = mapped
+	} else {
+		model = s.cfg.ChatModelDefault
+	}
+	body, _ = sjson.SetBytes(body, "model", model)
+	body, _ = sjson.DeleteBytes(body, "intent")
+
+	proxyUrl := s.cfg.ChatApiBase + "/chat/completions"
+	req, err := http.NewRequestWithContext(ctx, http.MethodPost, proxyUrl, io.NopCloser(bytes.NewBuffer(body)))
+	if nil != err {
+		c.AbortWithStatus(http.StatusInternalServerError)
+		return
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Authorization", "Bearer "+s.cfg.ChatApiKey)
+	if "" != s.cfg.ChatApiOrganization {
+		req.Header.Set("OpenAI-Organization", s.cfg.ChatApiOrganization)
+	}
+	if "" != s.cfg.ChatApiProject {
+		req.Header.Set("OpenAI-Project", s.cfg.ChatApiProject)
+	}
+
+	resp, err := s.client.Do(req)
+	if nil != err {
+		if errors.Is(err, context.Canceled) {
+			c.AbortWithStatus(http.StatusRequestTimeout)
+			return
+		}
+
+		log.Println("request conversation failed:", err.Error())
+		c.AbortWithStatus(http.StatusInternalServerError)
+		return
+	}
+	defer closeIO(resp.Body)
+
+	if resp.StatusCode != http.StatusOK { // log
+		body, _ := io.ReadAll(resp.Body)
+		log.Println("request completions failed:", string(body))
+
+		resp.Body = io.NopCloser(bytes.NewBuffer(body))
+	}
+
+	c.Status(resp.StatusCode)
+
+	contentType := resp.Header.Get("Content-Type")
+	if "" != contentType {
+		c.Header("Content-Type", contentType)
+	}
+
+	_, _ = io.Copy(c.Writer, resp.Body)
+}
+
+func (s *ProxyService) codeCompletions(c *gin.Context) {
+	ctx := c.Request.Context()
+
+	time.Sleep(100 * time.Millisecond)
+	if ctx.Err() != nil {
+		abortCodex(c, http.StatusRequestTimeout)
+		return
+	}
+
+	body, err := io.ReadAll(c.Request.Body)
+	if nil != err {
+		abortCodex(c, http.StatusBadRequest)
+		return
+	}
+
+	body, _ = sjson.DeleteBytes(body, "extra")
+	body, _ = sjson.DeleteBytes(body, "nwo")
+	body, _ = sjson.SetBytes(body, "model", "gpt-3.5-turbo-instruct")
+
+	proxyUrl := s.cfg.CodexApiBase + "/completions"
+	req, err := http.NewRequestWithContext(ctx, http.MethodPost, proxyUrl, io.NopCloser(bytes.NewBuffer(body)))
+	if nil != err {
+		abortCodex(c, http.StatusInternalServerError)
+		return
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Authorization", "Bearer "+s.cfg.CodexApiKey)
+	if "" != s.cfg.CodexApiOrganization {
+		req.Header.Set("OpenAI-Organization", s.cfg.CodexApiOrganization)
+	}
+	if "" != s.cfg.CodexApiProject {
+		req.Header.Set("OpenAI-Project", s.cfg.CodexApiProject)
+	}
+
+	resp, err := s.client.Do(req)
+	if nil != err {
+		if errors.Is(err, context.Canceled) {
+			abortCodex(c, http.StatusRequestTimeout)
+			return
+		}
+
+		log.Println("request completions failed:", err.Error())
+		abortCodex(c, http.StatusInternalServerError)
+		return
+	}
+	defer closeIO(resp.Body)
+
+	if resp.StatusCode != http.StatusOK {
+		body, _ := io.ReadAll(resp.Body)
+		log.Println("request completions failed:", string(body))
+
+		abortCodex(c, resp.StatusCode)
+		return
+	}
+
+	c.Status(resp.StatusCode)
+
+	contentType := resp.Header.Get("Content-Type")
+	if "" != contentType {
+		c.Header("Content-Type", contentType)
+	}
+
+	_, _ = io.Copy(c.Writer, resp.Body)
+}
+
+func main() {
+	cfg := readConfig()
+
+	gin.SetMode(gin.ReleaseMode)
+	r := gin.Default()
+
+	proxyService, err := NewProxyService(cfg)
+	if nil != err {
+		log.Fatal(err)
+		return
+	}
+
+	proxyService.InitRoutes(r)
+
+	err = r.Run(cfg.Bind)
+	if nil != err {
+		log.Fatal(err)
+		return
+	}
+}
